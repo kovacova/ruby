bd19d49bef027bfd0a6c9f91c61279398b2f19cb
Tina Kovacova <tina@kovacova.ca>
2021-10-01 01:33:55 -0700

    lesson 1

---
+++ 01-intro/001-ruby-quiz.rb
@@ -0,0 +1,11 @@
+# Define an array (an equivalent of Python list)
+address = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# P for print statements
+p address
+p address[5]
+
+# Reverse the array 
+new_address = address.reverse! 
+p new_address
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ruby
-Ruby + Rails practice
+# Ruby
+
+Ruby and Ruby on Rails practice @ [Udemy](https://www.udemy.com/course/the-complete-ruby-on-rails-developer-course).
+
+
