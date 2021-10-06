3b7458dc4e42b153625892c52cc98101e9180262
Tina Kovacova <tina@kovacova.ca>
2021-10-05 23:38:46 -0700

    lesson 1

---
+++ 02-ruby-lang/01-intro.rb
@@ -0,0 +1,19 @@
+
+# Put String = Print out String
+print "Hello World\n"
+
+puts "Hello World"      # Adds a newline
+
+p "Hello World"         # Doesn't return nil 
+
+greeting = "Hello World"
+
+puts greeting 
+
+# We always end the function with the end command
+def say_hello(thing_to_say)
+    puts thing_to_say
+end 
+
+say_hello("Hi")
+
