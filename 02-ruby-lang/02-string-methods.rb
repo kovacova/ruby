2d0dd61b1bb0b57e1ac87192d770804d2dc588c6
Tina Kovacova <tina@kovacova.ca>
2021-10-09 23:58:32 -0700

    string methods pt. 2 + homework

--- 02-ruby-lang/02-string-methods.rb
+++ 02-ruby-lang/02-string-methods.rb
@@ -20,3 +20,24 @@ puts first_name.to_s
 puts first_name.reverse
 
 # We can use the "\" - to escape things
+
+
+# Part 2
+puts "What is your first name?"
+first_name = gets.chomp # Works best with strings
+
+puts "Thank you, you said your first name is #{first_name}"
+
+
+# Homework
+puts "Enter your first name"
+first_name = gets.chomp
+
+puts "Enter your last name"
+last_name = gets.chomp
+
+full_name = first_name + " " + last_name
+
+puts "Your full name is #{full_name}"
+puts "Your full name reversed is #{full_name.reverse}"
+puts "Your name has #{full_name.length - 1} characters in it"
