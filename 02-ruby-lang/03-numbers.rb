1211da819911e4b15025211829180d3c09e5c652
Tina Kovacova <tina@kovacova.ca>
2021-10-29 23:45:13 -0700

    numbers extended

--- 02-ruby-lang/03-numbers.rb
+++ 02-ruby-lang/03-numbers.rb
@@ -1,7 +1,23 @@
-puts "-" * 20
+puts "-" * 25
 
 # Print doesn't end with a newline
-20.times {print "-"} 
+# 20.times {print "-"} 
 
 # Random characters 
-20.times {puts rand(10)}
+# 20.times {puts rand(10)}
+
+puts "Simple calculator" 
+
+25.times { print "-" } 
+puts # New line 
+
+puts "Enter the first number"
+num_1 = gets.chomp
+
+puts "Enter the second number" 
+num_2 = gets.chomp 
+
+
+# We need to convert the numbers to integers, but the best practice is to do it at the point of operations
+puts "The first number multiplied by the second number is #{num_1.to_i * num_2.to_i}"
+
